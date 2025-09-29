ba4a3d558a08f1e96faf4cc3178e9b180b8befcb
Patryk Gołębiowski <patryk.golebiowski1@outlook.com>
2025-09-29 18:30:55 +0200

    chore: clean up initialization
    - Remove Steam library initialization on connect

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,12 +1,9 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
 
-import { getSteam } from "./services/steam-singleton";
-
 import { BigPicture } from "./actions/big-picture";
 import { Status } from "./actions/status";
 import { RunApp } from "./actions/run-app";
 import { SwitchAccount } from "./actions/switch-account";
-
 
 // "trace" logging so that all messages between the Stream Deck, and the plugin are recorded.
 streamDeck.logger.setLevel(LogLevel.TRACE);
@@ -18,12 +15,4 @@ streamDeck.actions.registerAction(new RunApp());
 streamDeck.actions.registerAction(new SwitchAccount());
 
 // Connect to Stream Deck and initialize Steam library
-streamDeck.connect().then(async () => {
-  const steam = await getSteam();
-
-  streamDeck.settings.setGlobalSettings({
-    installedGames: steam.getInstalledGames(),
-  });
-  const settings = await streamDeck.settings.getGlobalSettings();
-  streamDeck.logger.info("Global settings:", settings);
-});
+streamDeck.connect().then(async () => {});
