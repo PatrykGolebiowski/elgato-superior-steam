b38c3120810b55cf5ce082315dd4da64bcb03e3b
Patryk Gołębiowski <patryk.golebiowski1@outlook.com>
2026-01-27 00:27:45 +0100

    feat: add native stream monitoring
    Added native monitoring for `steam.exe` by Elgato.
    
    See: https://docs.elgato.com/streamdeck/sdk/guides/app-monitoring

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,14 @@
-import streamDeck, { LogLevel } from "@elgato/streamdeck";
+import streamDeck, {
+  ApplicationDidLaunchEvent,
+  ApplicationDidTerminateEvent,
+  LogLevel,
+} from "@elgato/streamdeck";
 
 import { BigPicture } from "./actions/big-picture";
-import { Status } from "./actions/status";
-import { RunApp } from "./actions/run-app";
 import { LaunchAccount } from "./actions/launch-account";
+import { RunApp } from "./actions/run-app";
+import { resetSteam } from "./services/steam-singleton";
+import { Status } from "./actions/status";
 
 // "trace" logging so that all messages between the Stream Deck, and the plugin are recorded.
 streamDeck.logger.setLevel(LogLevel.TRACE);
@@ -13,6 +18,21 @@ streamDeck.actions.registerAction(new BigPicture());
 streamDeck.actions.registerAction(new Status());
 streamDeck.actions.registerAction(new RunApp());
 streamDeck.actions.registerAction(new LaunchAccount());
+
+streamDeck.system.onApplicationDidLaunch((ev: ApplicationDidLaunchEvent) => {
+  if (ev.application === "steam.exe") {
+    streamDeck.logger.info("Steam launched");
+  }
+});
+
+streamDeck.system.onApplicationDidTerminate(
+  (ev: ApplicationDidTerminateEvent) => {
+    if (ev.application === "steam.exe") {
+      streamDeck.logger.info("Steam terminated");
+      resetSteam();
+    }
+  },
+);
 
 // Connect to Stream Deck and initialize Steam library
 streamDeck.connect().then(async () => {});
