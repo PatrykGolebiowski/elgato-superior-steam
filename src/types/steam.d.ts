d415474fea0696256abfe13d895984803b1fca52
Patryk Gołębiowski <patryk.golebiowski1@outlook.com>
2026-01-26 18:53:11 +0100

    feat: add steam app states
    - Add `SteamStateFlags` enum with known state values.

--- src/types/steam.d.ts
+++ src/types/steam.d.ts
@@ -13,11 +13,40 @@ type SteamLibraryFolders = {
   apps: string[];
 };
 
+/**
+ * Steam app state flags (bitmask values from .acf manifests)
+ * @see https://github.com/lutris/lutris/blob/master/docs/steam.rst
+ */
+declare const enum SteamStateFlags {
+  Invalid = 0,
+  Uninstalled = 1,
+  UpdateRequired = 2,
+  FullyInstalled = 4,
+  Encrypted = 8,
+  Locked = 16,
+  FilesMissing = 32,
+  AppRunning = 64,
+  FilesCorrupt = 128,
+  UpdateRunning = 256,
+  UpdatePaused = 512,
+  UpdateStarted = 1024,
+  Uninstalling = 2048,
+  BackupRunning = 4096,
+  Reconfiguring = 65536,
+  Validating = 131072,
+  AddingFiles = 262144,
+  Preallocating = 524288,
+  Downloading = 1048576,
+  Staging = 2097152,
+  Committing = 4194304,
+  UpdateStopping = 8388608,
+}
+
 type SteamApp = {
   id: number;
   name: string;
   installDir: string;
-  stateFlags: string;
+  stateFlags: number;
 };
 
 type PluginGlobalSettings = {
